7b18636502669b79f256083fae572d427ae246ac
Juhász Viktória 13c haladó <116707126+JuhaszViktoriaa@users.noreply.github.com>
2025-09-08 13:13:45 +0200

    Add files via upload

---
+++ c0juhvik-1.dart
@@ -0,0 +1,29 @@
+void main() {
+  //1
+  String nev = "vasvari";
+  print("az iskola neve: $nev");
+  
+  //2
+  String iranyitoszam = "1234";
+  String utca = "gogol utca";
+  String telepules = "szeged";
+  String hazszam = "1";
+
+print("az iskola ir.szama, utcaja, telepulese, hazszama: $iranyitoszam, $utca, $telepules, $hazszam");
+
+  //3
+  int atlfo = 28;
+  
+  int kilencedikosztalyokszama = 4;
+  int kilencedikesekglobalisan = kilencedikosztalyokszama * atlfo;
+  
+  int masosztalyok = 3;
+  int tizedikesek = masosztalyok * atlfo;
+  int tizenegyedikesek = masosztalyok * atlfo;
+  int tizenkettedikesek = masosztalyok * atlfo;
+  
+  print("9. évfolyam diákjai: $kilencedikesekglobalisan fő");
+  print("10. évfolyam diákjai: $tizedikesek fő");
+  print("11. évfolyam diákjai: $tizenegyedikesek fő");
+  print("12. évfolyam diákjai: $tizenkettedikesek fő");
+}
