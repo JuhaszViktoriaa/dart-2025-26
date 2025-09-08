b229322df74939609376b16a57920f0d194f80f8
Juhász Viktória 13c haladó <116707126+JuhaszViktoriaa@users.noreply.github.com>
2025-09-08 13:29:01 +0200

    Add files via upload

---
+++ c0juhvik-2.dart
@@ -0,0 +1,15 @@
+//Hozz létre egy programot és végezd el az alábbi eladatoket:
+
+//1. Hozd létre a következő változókat: intValue, doubleValue, strValue, boolValue, melyek rendre, int, double, String, boolean típusúak és mindegyiknek adj egy-egy értéket!
+
+//2. A számok esetén végezd el a lehetséges műveleteket és írasd ki az eredményeket intResult és doubleResult értékként.
+
+//3. Írask ki a változók értékeit! A kiíratások előtt használj megjegyzéseket, hogy mit íratsz ki!
+
+//4. A boolResult legyen a boolValue negáltja és írasd ki a boolResult értékét!
+
+//5. Szorgalmi:
+//Végezz el további műveleteket és írasd ki az eredményeket!
+
+//Megjegyzés:
+//Mindegyik kiíratás legyek olvasható,. Azaz jelenjen meg, hogy mit íratsz ki és mennyi a kiírt érték.
